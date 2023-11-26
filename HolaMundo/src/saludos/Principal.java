46479af135a17058aa17eac59174f0448e88bb35
Francisco José Alcantarilla Calado <alcantarilla.cafra24@triana.salesianos.edu>
2023-11-26 12:33:12 +0100

    Primer commit

---
+++ HolaMundo/src/saludos/Principal.java
@@ -0,0 +1,13 @@
+package saludos;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("#########################");
+		System.out.println("Saludo");
+		System.out.println("#########################");
+	}
+
+}
