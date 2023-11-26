1d217472af4402a9acbc31d9ad341583f2351b05
Francisco José Alcantarilla Calado <alcantarilla.cafra24@triana.salesianos.edu>
2023-11-26 12:58:22 +0100

    Segundo commit

---
+++ HolaMundo/src/hello/Principal.java
@@ -0,0 +1,16 @@
+package hello;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("--------------------");
+		System.out.println("Hello world");
+		System.out.println("--------------------");
+		
+		
+		
+	}
+
+}
